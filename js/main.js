0f5d6aaf976bac8224957a74bbf16428efa8fa1d
Eleanor Davies <arisaie@Eleanors-Air.lan>
2022-01-25 14:50:37 +0000

    mollied

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var countDate = new Date(Date.UTC(2021, 8, 13, 2, 30, 0)).getTime();
+var countDate = new Date(Date.UTC(2022, 10, 22, 2, 30, 0)).getTime();
 
 function newYear() {
     var now = new Date().getTime();
